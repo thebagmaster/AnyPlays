7b63a3ddee7413a0b839c4a9244e65cc21437b73
thebagmaster <daniel.w.nichols@gmail.com>
2016-02-28 22:25:46 -0600

    -libold
    old unused files removed

--- libold/pause.py
+++
@@ -1,6 +0,0 @@
-import psutil
-
-PROCNAME = "DARKSOULS.exe"
-for proc in psutil.process_iter():
-    if proc.name() == PROCNAME:
-        proc.suspend()
--- libold/resume.py
+++
@@ -1,6 +0,0 @@
-import psutil
-
-PROCNAME = "Fallout3.exe"
-for proc in psutil.process_iter():
-    if proc.name() == PROCNAME:
-        proc.resume()
